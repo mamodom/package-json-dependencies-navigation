5a921f9af02b121579b0c215e85a71daf5518080
Maximo Dominguez <mamodom@gmail.com>
2018-11-01 20:57:53 -0400

    After struggling trying to open a link with a DefinitionProvider, I realized that what I needed was a DocumentLinkProvider

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,48 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import {
+  Position,
+  DocumentLinkProvider,
+  TextDocument,
+  ProviderResult,
+  DocumentLink,
+  Range,
+  Uri,
+  ExtensionContext,
+  languages,
+} from 'vscode';
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "code-npm-navigation" is now active!');
+class NpmDependenciesLinkProvider implements DocumentLinkProvider {
+  provideDocumentLinks(document: TextDocument): ProviderResult<DocumentLink[]> {
+    const documentText = document.getText();
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const { dependencies = {}, devDependencies = {} } = JSON.parse(
+      documentText
+    );
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    return Object.keys({ ...dependencies, ...devDependencies }).map(k => {
+      const keyIndex = documentText.indexOf(`"${k}"`);
+
+      const position = document.positionAt(keyIndex + 1);
+
+      return new DocumentLink(
+        new Range(
+          position,
+          new Position(position.line, position.character + k.length)
+        ),
+        Uri.parse(`https://www.npmjs.com/package/${k}`)
+      );
     });
-
-    context.subscriptions.push(disposable);
+  }
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+export function activate(context: ExtensionContext) {
+  const disposable = languages.registerDocumentLinkProvider(
+    { language: 'json', scheme: 'file', pattern: '**/package.json' },
+    new NpmDependenciesLinkProvider()
+  );
+
+  context.subscriptions.push(disposable);
 }
+
+export function deactivate() {}
